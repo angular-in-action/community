6df0263d410633665db349f86e3bd95d7fab86b2
Jeremy Wilken <gnomation@gnomeontherun.com>
2017-04-02 20:24:38 -0500

    implement logout

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   username: string = '';
   password: string = '';
   return: string = '';
 
-  ngOnInit() {}
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private route: ActivatedRoute) {}
 
-  login() {}
+  ngOnInit() {
+    this.route.queryParams
+      .subscribe(params => {
+        this.return = params['return'] || '/forums';
+        if (!this.userService.isGuest()) {
+          this.go();
+        }
+      });
+  }
+
+  login() {
+    if (this.username && this.password) {
+      this.userService.login(this.username);
+      this.go();
+    }
+  }
+
+  private go() {
+    this.router.navigateByUrl(this.return);
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 let username = localStorage.getItem('username') || '';
 let guest = (username) ? false : true;
@@ -6,7 +7,7 @@ let guest = (username) ? false : true;
 @Injectable()
 export class UserService {
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   isGuest() {
     return guest;
@@ -26,5 +27,6 @@ export class UserService {
     username = '';
     guest = true;
     localStorage.setItem('username', '');
+    this.router.navigate([{outlets: {chat: null}}]);
   }
 }
